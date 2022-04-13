9606e6e424adcfc6882cc9f8e1a98a688ddd4f7e
Troy <troy0h@hotmail.co.uk>
2022-04-13 21:41:31 +0100

    Added more tables

---
+++ hotelbooking/src/main/java/com/classes/Room.java
@@ -0,0 +1,9 @@
+package com.classes;
+
+public class Room {
+    public int roomNumber;
+    public String roomType; // Single, Twin or Double
+                            // Single = 1 single bed
+                            // Twin   = 2 single beds
+                            // Double = 1 double bed
+}
--- hotelbooking/src/main/java/com/hotelbooking/sql/SqlTables.java
+++ hotelbooking/src/main/java/com/hotelbooking/sql/SqlTables.java
@@ -1,6 +1,8 @@
 package com.hotelbooking.sql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import com.hotelbooking.DialogBox;
@@ -9,6 +11,8 @@ public class SqlTables {
     static void executeSQL(Connection conn) {
         createCustomersTable(conn);
         createStaffTables(conn);
+        createBookingTables(conn);
+        createRoomsTables(conn);
     }
 
     static void createCustomersTable(Connection conn) {
@@ -16,12 +20,14 @@ public class SqlTables {
             Statement stmt = conn.createStatement();
             // SQL Query to create "customers" table
             String query = "CREATE TABLE IF NOT EXISTS customers (" + 
-                "userId integer PRIMARY KEY," + 
+                "customerId integer PRIMARY KEY," + 
                 "username text UNIQUE NOT NULL," + 
                 "password text NOT NULL," +
                 "email text NOT NULL," +
                 "paymentMethod text NOT NULL," +
-                "corporateClient text NOT NULL );";
+                "corporateClient text NOT NULL," +
+                "totalTab int );";
+
 
             stmt.executeUpdate(query);
         }
@@ -35,7 +41,7 @@ public class SqlTables {
             Statement stmt = conn.createStatement();
             // SQL Query to create "staff" table
             String query = "CREATE TABLE IF NOT EXISTS staff (" + 
-                "staffId integer PRIMARY KEY," + 
+                "staffId int PRIMARY KEY," + 
                 "username text UNIQUE NOT NULL," + 
                 "password text NOT NULL," +
                 "staffType text NOT NULL );";
@@ -44,6 +50,124 @@ public class SqlTables {
         }
         catch (Exception ex) {
             DialogBox.Exception(ex);
+        }
+    }
+
+    static void createBookingTables(Connection conn) {
+        // Link table between rooms, customers and receptionists
+        try {
+            Statement stmt = conn.createStatement();
+            String query = "CREATE TABLE IF NOT EXISTS bookings (" +
+                "bookingId int PRIMARY KEY," +
+                "roomId int NOT NULL," + 
+                "staffId int NOT NULL," + 
+                "customerId int NOT NULL," +
+                "timeOfStart text NOT NULL," + // YYYY-MM-DD HH:MM:SS.SSS
+                "timeOfExit text NOT NULL," +  // YYYY-MM-DD HH:MM:SS.SSS
+                "bookingPrice real NOT NULL );";
+            stmt.executeUpdate(query);
+        }
+        catch (Exception ex) {
+            DialogBox.Exception(ex);
+        }
+    }
+
+    static void createRoomsTables(Connection conn) {
+        // Check if the table exists
+        boolean tExists = false;
+        try (ResultSet rs = conn.getMetaData().getTables(null, null, "rooms", null)) {
+            while (rs.next()) { 
+                String tName = rs.getString("TABLE_NAME");
+                if (tName != null && tName.equals("rooms")) {
+                    tExists = true;
+                }
+            }
+        }
+        catch (Exception ex) {
+            DialogBox.Exception(ex);
+        }
+        // If it doesnt, make the table...
+        if (tExists == false) {
+            try {
+                Statement stmt = conn.createStatement();
+                // SQL Query to create "rooms" table
+                String query = "CREATE TABLE IF NOT EXISTS rooms (" + 
+                    "roomId integer PRIMARY KEY," + 
+                    "roomNum int UNIQUE NOT NULL," + 
+                    "roomType text NOT NULL );";
+    
+                stmt.executeUpdate(query);
+            }
+            catch (Exception ex) {
+                DialogBox.Exception(ex);
+            }
+            
+            // ... And then populate it
+            try {
+                int n = 100;
+                PreparedStatement stmt = conn.prepareStatement("INSERT INTO rooms (roomNum, roomType) VALUES (?, ?)");
+                while (n <= 109) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Single");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                n = 200;
+                while (n <= 209) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Single");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                n = 300;
+                while (n <= 309) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Single");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                n = 400;
+                while (n <= 409) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Single");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                n = 500;
+                while (n <= 509) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Double");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                n = 600;
+                while (n <= 609) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Double");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                n = 700;
+                while (n <= 709) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Twin");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                n += 1;
+                n = 800;
+                while (n <= 809) {
+                    stmt.setInt(1, n);
+                    stmt.setString(2, "Twin");
+                    stmt.executeUpdate();
+                    n += 1;
+                }
+                
+            }
+            catch (Exception ex) {
+                DialogBox.Exception(ex);
+            }
+
         }
     }
 }
